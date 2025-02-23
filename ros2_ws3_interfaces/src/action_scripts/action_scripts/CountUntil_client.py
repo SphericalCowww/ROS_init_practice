cab1b6f94b223c8c4e28276d2af4f16e1436bc6f
SphericalCowww <tinglin194@gmail.com>
2025-02-23 23:31:04 +0100

    - debugged such that they can run

--- ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_client.py
+++ ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_client.py
@@ -3,43 +3,44 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle
-from practice_robot_interfaces.action import countUntil
+from practice_robot_interfaces.action import CountUntil
 
 #####################################################################################################
-class countUntil_serverNode(Node):
+class CountUntil_serverNode(Node):
     def __init__(self):
-        super().__init__("countUntil_client")
+        super().__init__("CountUntil_client")
         self.count_until_client_ = ActionClient(\
             self,\
             CountUntil,\
             "count_until") #NOTE: must be the same name as server
-        self.get_logger().info("countUntil_clientNode: action client has been started")
+        self.get_logger().info("CountUntil_clientNode: action client has been started")
     def send_goal(self, target_number, wait_time_per_count):
         self.count_until_client_.wait_for_server()
-        goal = countUntil.Goal()
+        goal = CountUntil.Goal()
         goal.target_number = target_number
         goal.wait_time_per_count = wait_time_per_count
 
-        self.get_logger().info("countUntil_clientNode: sending goal")
+        self.get_logger().info("CountUntil_clientNode: sending goal")
         # without async, only send after spin finishes; async is a python future object
         self.count_until_client_.send_goal_async(goal)\
                                 .add_done_callback(self.goal_response_callback)
     def goal_response_callback(self, futureObj):
         self.goal_handle_ : ClientGoalHandle = futureObj.result()
         if self.goal_handle_.accepted:
-            self.get_logger().info("countUntil_clientNode: goal accepted")
+            self.get_logger().info("CountUntil_clientNode: goal accepted")
             self.goal_handle_.get_result_async()\
                              .add_done_callback(self.goal_result_callback)
         else:
-            self.get_logger().warn("countUntil_clientNode: goal rejected")
+            self.get_logger().warn("CountUntil_clientNode: goal rejected")
     def goal_result_callback(self, futureObj):
         result = futureObj.result().result
-        self.get_logger().info("countUntil_clientNode: receiving result: "+str(result.reached_number))
+        self.get_logger().info("CountUntil_clientNode: receiving result: "+\
+                               str(result.reached_number))
         
 #####################################################################################################
 def main(args=None):
     rclpy.init(args=args)
-    node = countUntil_serverNode()
+    node = CountUntil_serverNode()
     node.send_goal(10, 1.0)             # client sending goal request to server
     rclpy.spin(node)                    # basically while 1==1
     rclpy.shutdown()
--- ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_server.py
+++ ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_server.py
@@ -4,43 +4,43 @@ import time
 from rclpy.node import Node
 from rclpy.action import ActionServer, GoalResponse
 from rclpy.action.server import ServerGoalHandle
-from practice_robot_interfaces.action import countUntil
+from practice_robot_interfaces.action import CountUntil
 
 #####################################################################################################
-class countUntil_serverNode(Node):
+class CountUntil_serverNode(Node):
     def __init__(self):
-        super().__init__("countUntil_server")
+        super().__init__("CountUntil_server")
         self.count_until_server_ = ActionServer(\
             self,\
             CountUntil,\
             "count_until",\
             goal_callback=self.goal_callback,                                    
             execute_callback=self.execute_callback)
-        self.get_logger().info("countUntil_serverNode: action server has been started")
-    def goal_callback(self, goal_request: countUntil.Goal):
-        self.get_logger().info("countUntil_serverNode: goal received")
-        if goal.request.target_number <= 0:
-            self.get_logger().info("countUntil_serverNode: rejecting goal")
+        self.get_logger().info("CountUntil_serverNode: action server has been started")
+    def goal_callback(self, goal_request: CountUntil.Goal):
+        self.get_logger().info("CountUntil_serverNode: goal received")
+        if goal_request.target_number <= 0:
+            self.get_logger().info("CountUntil_serverNode: rejecting goal")
             return GoalResponse.REJECT
-        self.get_logger().info("countUntil_serverNode: accepting goal") 
+        self.get_logger().info("CountUntil_serverNode: accepting goal") 
         return GoalResponse.ACCEPT
     def execute_callback(self, goal_handle: ServerGoalHandle):
         target_number       = goal_handle.request.target_number
         wait_time_per_count = goal_handle.request.wait_time_per_count
-        self.get_logger().info("countUntil_serverNode: executing the goal")
+        self.get_logger().info("CountUntil_serverNode: executing the goal")
         local_counter = 0
-        for counterIdx in range(target_number)
+        for counterIdx in range(target_number):
             local_counter += 1
-            self.get_logger().info("countUntil_serverNode: counting: "+str(local_counter))
+            self.get_logger().info("CountUntil_serverNode: counting: "+str(local_counter))
             time.sleep(wait_time_per_count)
         goal_handle.succeed()
-        result = countUntil.Result()
+        result = CountUntil.Result()
         result.reached_number = local_counter
         return result
 #####################################################################################################
 def main(args=None):
     rclpy.init(args=args)
-    node = countUntil_serverNode()
+    node = CountUntil_serverNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
