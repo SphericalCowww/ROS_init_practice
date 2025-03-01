f8345f1ad2751ee3273d3fa852ec157192ad0da6
SphericalCowww <tinglin194@gmail.com>
2025-03-01 18:22:02 +0100

    - reorganizing the code

--- ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_client.py
+++ ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_client.py
@@ -5,35 +5,30 @@ from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle, GoalStatus
 from practice_robot_interfaces.action import CountUntil
 
-#####################################################################################################
-class CountUntil_serverNode(Node):
+#######################################################################################################################
+class CountUntil_clientNode(Node):
     def __init__(self):
         super().__init__("CountUntil_client")
         self.goal_handle_ = None
         self.count_until_client_ = ActionClient(\
             self,\
             CountUntil,\
-            "count_until") #NOTE: must be the same name as server
+            "count_until") ### NOTE: must be the same name as server
         self.get_logger().info("CountUntil_clientNode: action client has been started")
     def send_goal(self, target_number, wait_time_per_count):
         self.count_until_client_.wait_for_server()
-        goal = CountUntil.Goal()
-        goal.target_number = target_number
+        goal                     = CountUntil.Goal()
+        goal.target_number       = target_number
         goal.wait_time_per_count = wait_time_per_count
 
         self.get_logger().info("CountUntil_clientNode: sending goal")
-        # without async, only send after spin finishes; async is a python future object
-        self.count_until_client_.send_goal_async(goal,feedback_callback=self.goal_feedback_callback)\
+        self.count_until_client_.send_goal_async(goal, feedback_callback=self.goal_feedback_callback)\
                                 .add_done_callback(self.goal_response_callback)
-
-        self.timer_ = self.create_timer(5.0, self.cancel_goal)
-    def cancel_goal(self):
-        self.get_logger().info("CountUntil_clientNode: sending cancel request")
-        self.goal_handle_.cancel_goal_async()
-        self.timer_.cancel()
-
+        #self.timer_ = self.create_timer(5.0, self.cancel_goal)             ### for test cancel
+    def goal_feedback_callback(self, feedback_msg):
+        self.get_logger().info("CountUntil_clientNode: receiving feedback: "+str(feedback_msg.feedback.current_number))
     def goal_response_callback(self, futureObj):
-        self.goal_handle_: ClientGoalHandle = futureObj.result()
+        self.goal_handle_:ClientGoalHandle = futureObj.result()
         if self.goal_handle_.accepted:
             self.get_logger().info("CountUntil_clientNode: goal accepted")
             self.goal_handle_.get_result_async()\
@@ -43,26 +38,23 @@ class CountUntil_serverNode(Node):
     def goal_result_callback(self, futureObj):
         status = futureObj.result().status
         result = futureObj.result().result
-        if status == GoalStatus.STATUS_SUCCEEDED:
-            self.get_logger().info("CountUntil_clientNode: goal succeeded")
-        elif status == GoalStatus.STATUS_ABORTED:
-            self.get_logger().error("CountUntil_clientNode: goal aborted")
-        elif status == GoalStatus.STATUS_CANCELED:
-            self.get_logger().error("CountUntil_clientNode: goal canceled")
-        self.get_logger().info("CountUntil_clientNode: receiving result: "+\
-                               str(result.reached_number))
-    def goal_feedback_callback(self, feedback_msg):
-        self.get_logger().info("CountUntil_clientNode: receiving feedback: "+\
-                               str(feedback_msg.feedback.current_number)) 
-#####################################################################################################
+        if   status == GoalStatus.STATUS_SUCCEEDED: self.get_logger().info( "CountUntil_clientNode: goal succeeded")
+        elif status == GoalStatus.STATUS_ABORTED:   self.get_logger().error("CountUntil_clientNode: goal aborted")
+        elif status == GoalStatus.STATUS_CANCELED:  self.get_logger().error("CountUntil_clientNode: goal canceled")
+        self.get_logger().info("CountUntil_clientNode: receiving result: "+str(result.reached_number))
+    def cancel_goal(self):
+        self.get_logger().info("CountUntil_clientNode: sending cancel request")
+        self.goal_handle_.cancel_goal_async()
+        #self.timer_.cancel()                                               ### for test cancel
+#######################################################################################################################
 def main(args=None):
     rclpy.init(args=args)
-    node = CountUntil_serverNode()
+    node = CountUntil_clientNode()
     node.send_goal(10, 1.0)             # client sending goal request to server
     rclpy.spin(node)                    # basically while 1==1
     rclpy.shutdown()
 
-#####################################################################################################
+#######################################################################################################################
 if __name__ == "__main__": main()
 
 
--- ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_server.py
+++ ros2_ws3_interfaces/src/action_scripts/action_scripts/CountUntil_server.py
@@ -9,12 +9,12 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import ReentrantCallbackGroup
 from practice_robot_interfaces.action import CountUntil
 
-#####################################################################################################
+#######################################################################################################################
 class CountUntil_serverNode(Node):
     def __init__(self):
         super().__init__("CountUntil_server")
         self.goal_handle_: ServerGoalHandle = None
-        self.goal_lock_ = threading.Lock()
+        self.goal_lock_  = threading.Lock()
         self.goal_queue_ = []
         self.count_until_server_ = ActionServer(\
             self,\
@@ -22,27 +22,26 @@ class CountUntil_serverNode(Node):
             "count_until",\
             goal_callback            = self.goal_callback,\
             handle_accepted_callback = self.handle_accepted_callback,\
-            cancel_callback          = self.cancel_callback,\
             execute_callback         = self.execute_callback,\
+            cancel_callback          = self.cancel_callback,\
             callback_group           = ReentrantCallbackGroup())
         self.get_logger().info("CountUntil_serverNode: action server has been started")
-    def goal_callback(self, goal_request:CountUntil.Goal):
+    def goal_callback(self, goal:CountUntil.Goal):
         self.get_logger().info("CountUntil_serverNode: goal received")
-        if goal_request.target_number <= 0:
+        if goal.target_number <= 0:
             self.get_logger().info("CountUntil_serverNode: rejecting goal")
             return GoalResponse.REJECT        
 
         '''
+        ### for test cancel
         with self.goal_lock_:
             if self.goal_handle_ is not None:
                 if self.goal_handle_.is_active == True:
                     ### cancel new goal if multiple goal
-                    #self.get_logger().info("CountUntil_serverNode: a goal is already active, "+\
-                    #                       "rejecting new goal")
+                    #self.get_logger().info("CountUntil_serverNode: a goal is already active, rejecting new goal")
                     #return GoalResponse.REJECT
                     ### abort current goal if multiple goal
-                    self.get_logger().info("CountUntil_serverNode: abort current goal, "+\
-                                           "accepting new goal")
+                    self.get_logger().info("CountUntil_serverNode: abort current goal, accepting new goal")
                     self.goal_handle_.abort()
         '''
         
@@ -56,50 +55,49 @@ class CountUntil_serverNode(Node):
             else:
                 self.get_logger().info("CountUntil_serverNode: goal queue first execute")
                 goal_handle.execute()   
-    def cancel_callback(self, goal_handle:ServerGoalHandle):
-        self.get_logger().info("CountUntil_serverNode: receiving cancel request")
-        return CancelResponse.ACCEPT
     def execute_callback(self, goal_handle:ServerGoalHandle):
         with self.goal_lock_:
             self.goal_handle_ = goal_handle
-
-        target_number       = goal_handle.request.target_number
-        wait_time_per_count = goal_handle.request.wait_time_per_count
-        result   = CountUntil.Result()
-        feedback = CountUntil.Feedback()        
+        goalVars     = goal_handle.request                          ### object of Goal() varaiables
+        feedbackVars = CountUntil.Feedback()                        ### object of Feedback() varaiables
+        resultVars   = CountUntil.Result()                          ### object of Result() varaiables
 
         self.get_logger().info("CountUntil_serverNode: executing the goal")
         local_counter = 0
-        for counterIdx in range(target_number):
+        for counterIdx in range(goalVars.target_number):
             if goal_handle.is_active == False:
                 self.get_logger().info("CountUntil_serverNode: aborting the goal")
-                result.reached_number = local_counter
+                resultVars.reached_number = local_counter
                 self.process_next_goal_in_queue()
-                return result
+                return resultVars
             if goal_handle.is_cancel_requested == True:
                 self.get_logger().info("CountUntil_serverNode: canceling the goal")
                 goal_handle.canceled()
-                result.reached_number = local_counter
+                resultVars.reached_number = local_counter
                 self.process_next_goal_in_queue()
-                return result
+                return resultVars
             local_counter += 1
-            feedback.current_number = local_counter
-            goal_handle.publish_feedback(feedback)
             self.get_logger().info("CountUntil_serverNode: counting: "+str(local_counter))
-            time.sleep(wait_time_per_count)
+            feedbackVars.current_number = local_counter
+            goal_handle.publish_feedback(feedbackVars)
+            time.sleep(goalVars.wait_time_per_count)
         self.get_logger().info("CountUntil_serverNode: goal succeeded")
         goal_handle.succeed()
-        result.reached_number = local_counter
+        resultVars.reached_number = local_counter
         self.process_next_goal_in_queue()
-        return result
+        return resultVars
     def process_next_goal_in_queue(self):
+        ### honestly sequentialize goals, more or less defeat the purpose of ReentrantCallbackGroup?
         with self.goal_lock_:
             if len(self.goal_queue_) > 0:
                 self.get_logger().info("CountUntil_serverNode: goal queue pop")
                 self.goal_queue_.pop(0).execute()
             else:
                 self.goal_handle_: ServerGoalHandle = None
-#####################################################################################################
+    def cancel_callback(self, goal_handle:ServerGoalHandle):
+        self.get_logger().info("CountUntil_serverNode: receiving cancel request")
+        return CancelResponse.ACCEPT
+#######################################################################################################################
 def main(args=None):
     rclpy.init(args=args)
     node = CountUntil_serverNode()
@@ -111,6 +109,6 @@ def main(args=None):
 
 
 
-#####################################################################################################
+#######################################################################################################################
 if __name__ == "__main__": main()
 
