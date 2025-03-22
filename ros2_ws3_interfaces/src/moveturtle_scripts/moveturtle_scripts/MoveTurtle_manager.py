e9c4d71476d42c374c81a254c67473f4fa1f33a9
SphericalCowww <tinglin194@gmail.com>
2025-03-22 11:40:50 +0100

    - adding the turtlesim based launcher first

--- ros2_ws3_interfaces/src/moveturtle_scripts/moveturtle_scripts/MoveTurtle_manager.py
+++ ros2_ws3_interfaces/src/moveturtle_scripts/moveturtle_scripts/MoveTurtle_manager.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 import rclpy
 import time
+import numpy as np
 from rclpy.node import Node
-from lifecycle_msgs.srv import ChangeState
-from lifecycle_msgs.msg import Transition
+from rclpy.callback_groups import ReentrantCallbackGroup
+from turtlesim.srv import Spawn, Kill
 
 #######################################################################################################################
 class MoveTurtle_manager(Node):
     def __init__(self):
         super().__init__("MoveTurtle_manager")
-        self.declare_parameter("managed_node_names", rclpy.Parameter.Type.STRING_ARRAY)
-        self.managed_node_names_ = self.get_parameter("managed_node_names").value
-        self.get_logger().info("MoveTurtle_manager: starting lifecycle manager")
-        self.MoveTurtle_managers_ = {}
-        for nodeName in self.managed_node_names_:
-            self.get_logger().info("MoveTurtle_manager: loading "+str(nodeName))
-            self.MoveTurtle_managers_[nodeName] = self.create_client(ChangeState, "/"+nodeName+"/change_state")
-    def change_state(self, transition:Transition):
-        for nodeName in self.managed_node_names_:
-            self.get_logger().info("MoveTurtle_manager: changing state")
-            self.MoveTurtle_managers_[nodeName].wait_for_service()
-            request = ChangeState.Request()
-            request.transition = transition
-            future = self.MoveTurtle_managers_[nodeName].call_async(request)
-            rclpy.spin_until_future_complete(self, future)
-    def initialization_sequence(self):
-        self.get_logger().info("MoveTurtle_manager: configuring")
-        transition = Transition()
-        transition.id = Transition.TRANSITION_CONFIGURE
-        transition.label = "configure"
-        self.change_state(transition)
-        self.get_logger().info("MoveTurtle_manager: configuring complete, now at inactive")
+        self.callback_group_ = ReentrantCallbackGroup()
+        self.spawn_client_ = self.create_client(Spawn, "spawn", callback_group=self.callback_group_)
+        self.kill_client_  = self.create_client(Kill,  "kill",  callback_group=self.callback_group_)
+        while not self.spawn_client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("MoveTurtle_manager: waiting for spawn service...")
+        self.spawn_turtle('this_turtle')
+        time.sleep(5.0)
+        self.kill_turtle('this_turtle')
+    def spawn_turtle(self, turtle_name):
+        request = Spawn.Request()
+        request.x     = 4.0
+        request.y     = 6.0
+        request.theta = np.pi/2.0
+        request.name = turtle_name
 
-        self.get_logger().info("MoveTurtle_manager: activating")
-        transition = Transition()
-        transition.id = Transition.TRANSITION_ACTIVATE
-        transition.label = "activate"
-        self.change_state(transition)
-        self.get_logger().info("MoveTurtle_manager: activating complete, now at active")
+        future = self.spawn_client_.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+
+        if future.result() is not None:
+            self.get_logger().info("MoveTurtle_manager: spawning turtle "+turtle_name)
+        else:
+            self.get_logger().error("MoveTurtle_manager: spawning fails")
+    def kill_turtle(self, turtle_name):
+        request = Kill.Request()    
+        request.name = turtle_name
+        
+        future = self.kill_client_.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        
+        if future.result() is not None:
+            self.get_logger().info("MoveTurtle_manager: killing turtle "+turtle_name)
+        else:
+            self.get_logger().error("MoveTurtle_manager: killing fails")
+
 #######################################################################################################################
 def main(args=None):
     rclpy.init(args=args)    
     node = MoveTurtle_manager()
-    node.initialization_sequence()
+    rclpy.spin(node)
     rclpy.shutdown()
 #######################################################################################################################
 if __name__ == "__main__": main()
