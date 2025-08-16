a2092eb5c3addf793efa2df5b6fd886d693000c5
SphericalCowww <tinglin194@gmail.com>
2025-08-16 20:51:57 +0200

    - some reorganization

---
+++ ros2_ws4_controller/src/my_robot_firmware_py/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_firmware_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kali',
+    maintainer_email='tinglin194@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'Arduino_serial_publisher = my_robot_firmware_py.Arduino_serial_publisher',
+            'Arduino_serial_receiver  = my_robot_firmware_py.Arduino_serial_receiver',
+            'Arduino_serial_lifecycle = my_robot_firmware_py.Arduino_serial_lifecycle',
+        ],
+    },
+)
