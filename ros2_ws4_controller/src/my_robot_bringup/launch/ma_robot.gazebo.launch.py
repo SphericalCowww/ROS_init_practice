bb6b6edb25071dfc5ef4461c3753fca0c9d3c360
SphericalCowww <tinglin194@gmail.com>
2025-08-31 21:32:56 +0200

    - now about to run launch ma_robot.gazebo.launch.py with gazebo running and linked to the controller

--- ros2_ws4_controller/src/my_robot_bringup/launch/ma_robot.gazebo.launch.py
+++ ros2_ws4_controller/src/my_robot_bringup/launch/ma_robot.gazebo.launch.py
@@ -3,6 +3,8 @@ from launch import LaunchDescription
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 ######################################################################################################################
@@ -10,8 +12,8 @@ def generate_launch_description():
     robot_description_path = get_package_share_path('my_robot_description')
     robot_bringup_path     = get_package_share_path('my_robot_bringup')
     
-    urdf_path          = os.path.join(robot_description_path, 'urdf', 'ma_robot.urdf.xacro')
-    gazebo_config_path = os.path.join(robot_bringup, 'config', 'gazebo_bridge.yaml')
+    urdf_path          = os.path.join(robot_description_path, 'urdf', 'ma_robot.gazebo.xacro')
+    gazebo_config_path = os.path.join(robot_bringup_path, 'config', 'gazebo_bridge.yaml')
     rviz_config_path   = os.path.join(robot_description_path, 'rviz', 'urdf_config.rviz')
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
     robot_controllers = os.path.join(robot_bringup_path, 'config', 'ma_robot_controllers.yaml')
@@ -21,18 +23,18 @@ def generate_launch_description():
         executable="robot_state_publisher",
         parameters=[{'robot_description': robot_description}],
     )
-    # find physics plugins in: /opt/ros/jazzy/opt/gz_physics_vendor/lib/gz-physics-7/engine-plugins
+    
+    # normal mode: -v 4 -r empty.sdf
+    # headless mode: -r -v 0 empty.sdf 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory("ros_gz_sim"),
+                get_package_share_path("ros_gz_sim"),
                 "launch",
                 "gz_sim.launch.py"
             ),
         ),
-        launch_arguments=[
-            ("gz_args", [" -v 4 -r empty.sdf --physics-engine libgz-physics7-bullet-featherstone-plugin"]),
-        ],
+        launch_arguments=[("gz_args", [" -r -v 0 empty.sdf"]),],
     )
     gz_spawn_entity = Node(
         package="ros_gz_sim",
