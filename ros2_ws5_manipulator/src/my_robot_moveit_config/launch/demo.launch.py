8db2ea6b8ba7deb17abd730384feec65828c78a2
SphericalCowww <tinglin194@gmail.com>
2025-09-20 22:57:45 +0200

    - reorganization

--- ros2_ws5_manipulator/src/my_robot_moveit_config/launch/demo.launch.py
+++ ros2_ws5_manipulator/src/my_robot_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robot_bringup").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robot_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- ros2_ws5_manipulator/src/my_robot_moveit_config/launch/moveit_rviz.launch.py
+++ ros2_ws5_manipulator/src/my_robot_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robot_bringup").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("my_robot", package_name="my_robot_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
